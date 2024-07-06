ba303a419f37afd79ef23cb6944bf1feea6fe5be
luh <lucineia.rsilvah@gmail.com>
2024-07-05 21:52:25 -0300

    animaçao do menu corrigida, e responsivo tablet inserido

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const menuIcon = document.querySelector('.fa-bars');
 const menu = document.querySelector('.menu__lista');
 menuIcon.addEventListener('click',()=>{
-menu.classList.toggle('menu__close');
+menu.classList.toggle('open');
 });
