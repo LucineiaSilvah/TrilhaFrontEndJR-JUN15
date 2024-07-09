780a3d648d92727c4005416a9d7782a0f6aebaeb
luh <lucineia.rsilvah@gmail.com>
2024-07-09 17:09:29 -0300

    add logo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 const menuIcon = document.querySelector('.fa-bars');
 const menu = document.querySelector('.menu__lista');
+const mode = document.querySelector('.mode');
+const modeIcon = document.querySelector('.fa-toggle-off');
+const sessoes = document.querySelectorAll('.darkMode');
+/* funcao abrir e fechar menu */
 menuIcon.addEventListener('click',()=>{
 menu.classList.toggle('open');
 });
+/* funcao modo dark ou ligth */
+
+mode.addEventListener('click', ()=>{
+sessoes.forEach(sessao =>{
+    sessao.classList.toggle('whiteMode');
+    
+})
+modeIcon.classList.toggle(`fa-toggle-on`)
+ 
+})
